99dc040f8b4bd209a9d778d1644e0c6aeda7f245
tonkaew131 <nongtonkaew@gmail.com>
2022-06-02 23:18:22 +0700

    Moved all features into src/ folder

--- make.sh
+++ make.sh
@@ -1,13 +1,13 @@
 total=3
 
 printf '[1/'$total'] Compiling pdf2png.py ...\n'
-python -m PyInstaller pdf2png.py -F --clean --noupx
+python -m PyInstaller src/pdf2png.py -F --clean --noupx
 printf '[1/'$total'] Done.\n\n\n\n\n'
 
 printf '[2/'$total'] Compiling png2pdf.py...\n'
-python -m PyInstaller png2pdf.py -F --clean --noupx
+python -m PyInstaller src/png2pdf.py -F --clean --noupx
 printf '[2/'$total'] Done.\n\n\n\n\n'
 
 printf '[3/'$total'] Compiling webp2png.py...\n'
-python -m PyInstaller webp2png.py -F --clean --noupx
+python -m PyInstaller src/webp2png.py -F --clean --noupx
 printf '[3/'$total'] Done.\n\n\n\n\n'
